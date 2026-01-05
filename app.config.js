ee4a8f6871bda1886d0e597de6ae97b01e91ce1b
Pranay Chitare <chitarepranay85@gmail.com>
2026-01-06 01:49:35 +0530

    appb

--- app.config.js
+++ app.config.js
@@ -15,11 +15,14 @@ export default {
       geminiApiKey: process.env.GEMINI_API_KEY,
       apiUrl: process.env.API_URL || 'http://localhost:3000',
       azureVisionEndpoint: process.env.AZURE_VISION_ENDPOINT || 'https://minutemeals-vision.cognitiveservices.azure.com',
-      azureVisionKey: process.env.AZURE_VISION_KEY
+      azureVisionKey: process.env.AZURE_VISION_KEY,
+      eas: {
+        projectId: "70c64346-e14e-432b-b114-44d5e20b6d73"
+      }
     },
     splash: { image: "./assets/splash-icon.png", resizeMode: "contain", backgroundColor: "#ffffff" },
-    ios: { supportsTablet: true },
-    android: { adaptiveIcon: { foregroundImage: "./assets/adaptive-icon.png", backgroundColor: "#ffffff" }, edgeToEdgeEnabled: true },
+    ios: { supportsTablet: true, bundleIdentifier: "com.pranay456.minutemeals" },
+    android: { adaptiveIcon: { foregroundImage: "./assets/adaptive-icon.png", backgroundColor: "#ffffff" }, edgeToEdgeEnabled: true, package: "com.pranay456.minutemeals" },
     web: { favicon: "./assets/favicon.png" }
   }
 }
